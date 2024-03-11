e7d0f628b7a8cca3e4fe3406fb0289a063965fcc
unknown <matheus.duartesilva42@gmail.com>
2024-03-11 16:22:27 -0300

    implementação das rotas get e post, dos repositories, routes e usecases e método create

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -2,4 +2,15 @@ export interface User {
     id: string
     email: string
     name: string
+    createdAt: Date
+    updatedAt: Date
+}
+
+export interface UserCreate {
+    email: string
+    name: string
+}
+
+export interface UserRepository {
+    create(data: UserCreate): Promise<User>
 }
